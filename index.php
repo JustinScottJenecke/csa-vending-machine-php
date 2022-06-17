98023caa89804339d1b079752cc01094ce8745c4
justinjenecke <justinjenecke@gmail.com>
2022-06-17 09:23:39 +0200

    creates page seperation, js files, and pages for main parts of App View

--- index.php
+++ index.php
@@ -6,12 +6,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Vending Machine</title>
+        <link rel="stylesheet" href="./public/style.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.9.4/css/bulma.min.css" integrity="sha512-HqxHUkJM0SYcbvxUw5P60SzdOTy/QVwA1JJrvaXJv4q7lmbDZCmZaqz01UPOaQveoxfYRv1tHozWGPMcuTBuvQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     </head>
 
     <body>
         
-        hi
+        <div>
+            <main>
+                
+            </main>
+        </div>
 
     </body>
 
